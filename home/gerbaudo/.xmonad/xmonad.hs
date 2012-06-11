27927c21ae8c17e2f9a3bd0cbdc51946b0b6eb78
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2012-06-11 07:53:52 +0200

    add multimedia keys (few of them not working yet)

--- home/gerbaudo/.xmonad/xmonad.hs
+++ home/gerbaudo/.xmonad/xmonad.hs
@@ -6,10 +6,15 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spiral
 import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig
+--import XMonad.Util.EZConfig
+--import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeysP, additionalKeys)
 import XMonad.Util.Run
 import System.IO
 
+--
+-- https://github.com/vicfryzel/xmonad-config/blob/master/xmonad.hs
+-- https://github.com/betchart/configs/blob/master/home/burt/.xmonad/xmonad.hs
 mylayoutHook = spiral (6/7) ||| Mirror tiled ||| Full
                where tiled = Tall 1 0.03 0.5 
 -- mylayoutHook = Mirror tiled ||| Full
@@ -41,8 +46,16 @@ xmonad $ defaultConfig
                  , ppLayout = const "" -- to disable the layout info on xmobar
                  }
        }
-       `additionalKeys` -- myKeys
-        [((mod1Mask .|. controlMask, xK_l), spawn "slock")] --
+       `additionalKeysP` -- myKeys
+        -- Extra keys : from xev and then /usr/include/X11/XF86keysym.h
+        [("<XF86AudioMute>",        spawn "amixer -q set Master toggle")
+        ,("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%- unmute")
+        ,("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+ unmute")
+        ,("<XF86Suspend>",          spawn "sudo /usr/sbin/pm-suspend")
+        ,("<XF86TouchpadToggle>",   spawn "/usr/local/bin/toggleTouchPad")
+        ,("<XF86ScreenSaver>",      spawn "/usr/bin/slock")
+        ,("<XF86Display>",          spawn "/usr/local/bin/toggleVga1")
+        ]
 --        ++
 --        [ (otherModMasks ++ "M-" ++ [key], action tag)
 --          | (tag, key)  <- zip myWorkspaces "12345678"
